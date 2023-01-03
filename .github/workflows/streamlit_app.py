0dcc70bb690f85d9212757dc7e1c3c75c81e30d4
riyaprakash <74323094+riyaprakash@users.noreply.github.com>
2023-01-03 13:11:29 -0500

    Update streamlit_app.py

--- .github/workflows/streamlit_app.py
+++ .github/workflows/streamlit_app.py
@@ -1,3 +1,6 @@
+import streamlit as st
+st.write("Hello ,let's learn how to build a streamlit app together")
+
 st.title ("this is the app title")
 st.header("this is the markdown")
 st.markdown("this is the header")
