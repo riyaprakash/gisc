af504979b1a4ff443c63275ff0a21a9590fd2fbb
riyaprakash <74323094+riyaprakash@users.noreply.github.com>
2023-01-09 18:23:40 -0500

    format

--- .github/workflows/stream_ourapp.py
+++ .github/workflows/stream_ourapp.py
@@ -10,68 +10,40 @@ with st.container():
     st.title("Welcome to the Girl in Space Club Outfitter")
     st.subheader("Enter your information below and continue to sizing to design your flight suit")
 
-# ---- CUSTOMER INFO ----
-with st.container():
-    st.write("---")
-    ##create 2 columns
-    left_column, right_column = st.columns(2)
-    with left_column:
-        ##Print customer info questions
-        st.header("Customer Info")
-        st.write("What is your name?")
-        st.text_input()
-        st.write("What is your email?")
-        st.text_input()
-        st.write("For what purpose are you purchasing this suit?")
-        st.text_input()
-    
-
 # ---- SIZING PREFERENCE ----
 with st.container():
     st.write("---")
     ##create 2 columns
-    left_column, right_column = st.columns(2)
-    with left_column:
+    with st.form("size"):
         ##Print size questions 
         st.header("Sizing Preference")
         st.write("##")
 
-        st.write("Do you want to purchase a space suit with custom measurements or with standard sizing?")
-        st.write("Custom     Standard")
+        st.selectbox("Do you want to purchase a space suit with custom measurements or with standard sizing?",
+        ("Custom", "Standard"))
         
         ##If user chooses custom measurements
-        st.write("What is your preferred unit of measurement?")
-        st.write("cm     in")
+        st.selectbox("What is your preferred unit of measurement?",("cm", "in"))
 
         ##If user chooses cm then convert the answers to inches
 
-        st.write("Enter height")
-        st.text_input()
-        st.write("Enter chest")
-        st.text_input()
-        st.write("Enter waist")
-        st.text_input()
-        st.write("Enter hips/seat")
-        st.text_input()
-        st.write("Enter total arm length")
-        st.text_input()
-        st.write("Enter inseam")
-        st.text_input()
-        st.write("Enter body length")
-        st.text_input()
+        st.text_input("Enter Height")
+        st.text_input("Enter Chest")
+        st.text_input("Enter Waist")
+        st.text_input("Enter waist")
+        st.text_input("Enter Total Arm Length")
+        st.text_input("Enter Inseam")
+        st.text_input("Enter Body Length")
 
         ##If user chooses standard sizing
-        st.write("Which size suit do you want to purchase?")
-        st.write("XXS  XS  S  M  L  XL")
+        st.selectbox("Which size suit do you want to purchase?",("XXS", "XS", "S", "M", "L", "XL"))
 
 
 
 # ---- SUIT COLOR ----
 with st.container():
     st.write("---")
-    ##create 2 columns
-    left_column, right_column = st.columns(2)
-    with left_column:
+    with st.form("color"):
         ##Print color questions
         st.header("Suit Color")
         st.write("##")
@@ -80,9 +52,7 @@ with st.container():
 # ---- PATCHES ----
 with st.container():
     st.write("---")
-    ##create 2 columns
-    left_column, right_column = st.columns(2)
-    with left_column:
+    with st.form("patches"):
         ##Print color questions
         st.header("Patches")
         st.write("##")
@@ -90,8 +60,8 @@ with st.container():
         st.button("Yes")
         st.button("No")
         st.write("We are currently unable to provide custom patches. However, if you would like to get your own, you have the option of including three blank patch spaces for the following dimensions:")
-        st.write("2 circular patches (diameter 3.75': right chest, left arm")
-        st.write("1 rectangular patch (2 x 4): left chest")
+        st.checkbox("2 circular patches (diameter 3.75': right chest, left arm")
+        st.checkbox("1 rectangular patch (2 x 4): left chest")
 
 
 # ---- FINISH ORDERING ----
