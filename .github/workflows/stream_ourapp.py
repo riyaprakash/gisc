6c46131a4ff0a5c66135cd0ee07849090f182e27
riyaprakash <74323094+riyaprakash@users.noreply.github.com>
2023-01-09 18:03:11 -0500

    error

--- .github/workflows/stream_ourapp.py
+++ .github/workflows/stream_ourapp.py
@@ -5,22 +5,6 @@ import streamlit as st
 # Find more emojis here: https://www.webfx.com/tools/emoji-cheat-sheet/
 st.set_page_config(page_title="GISC Flight Suit Outfitter", page_icon= ":rocket:", layout="wide")
 
-
-def load_lottieurl(url):
-    r = requests.get(url)
-    if r.status_code != 200:
-        return None
-    return r.json()
-
-
-# Use local CSS
-def local_css(file_name):
-    with open(file_name) as f:
-        st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
-
-
-local_css("style/style.css")
-
 # ---- HEADER SECTION ----
 with st.container():
     st.title("Welcome to the Girl in Space Club Outfitter")
