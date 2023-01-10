57d0797cecb165884e82d86d19d4b360cc660fc6
riyaprakash <74323094+riyaprakash@users.noreply.github.com>
2023-01-10 14:18:53 -0500

    moving files

---
+++ pages/2_Cart.py
@@ -0,0 +1,18 @@
+import requests
+import streamlit as st
+
+
+# Find more emojis here: https://www.webfx.com/tools/emoji-cheat-sheet/
+st.set_page_config(page_title="GISC Flight Suit Outfitter", page_icon= ":rocket:", layout="wide")
+
+# ---- HEADER SECTION ----
+with st.container():
+    st.title("Welcome to the Girl in Space Club Outfitter")
+    st.subheader("CART PAGE")
+
+# ---- CUSTOMER INFO ----
+with st.form("login"):
+    st.text_input("Username")
+    st.text_input("Password")
+    st.form_submit_button("Login")
+    st.form_submit_button("Create New Account")
