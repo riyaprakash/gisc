92d8d91d9fd63d5848f8d7a912c84a7e97b973db
riyaprakash <74323094+riyaprakash@users.noreply.github.com>
2023-01-10 07:56:07 -0500

    -

--- .github/workflows/new_account.py
+++ .github/workflows/new_account.py
@@ -1,20 +1,8 @@
 import requests
 import streamlit as st
-from streamlit_lottie import st_lottie
-from PIL import Image
-
 
 # Find more emojis here: https://www.webfx.com/tools/emoji-cheat-sheet/
 st.set_page_config(page_title="GISC Flight Suit Outfitter", page_icon= ":rocket:", layout="wide")
-
-
-# Use local CSS
-def local_css(file_name):
-    with open(file_name) as f:
-        st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
-
-
-local_css("style/style.css")
 
 
 # ---- HEADER SECTION ----
@@ -25,12 +13,9 @@ with st.container():
 # ---- CUSTOMER INFO ----
 with st.form("new_account"):
     ##Print customer info questions
-    st.write("What is your name?")
-    st.text_input()
-    st.write("What is your email?")
-    st.text_input()
-    st.write("For what purpose are you purchasing this suit?")
-    st.text_input()
+    st.text_input("What is your name?")
+    st.text_input("What is your email?")
+    st.text_input("What purpose are you purchasing this suit?")
     st.write("Enter your password")
     st.write(
         """
@@ -41,7 +26,6 @@ with st.form("new_account"):
         -One Special Character
         """
     )
-    st.text_input()
-    st.write("Re-enter your password")
-    st.text_input()
+    st.text_input("")
+    st.text_input("Re-enter yout password")
     st.form_submit_button("Create New Account")
