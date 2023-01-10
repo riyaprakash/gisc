789eec6c049d4f3b521ce33a55314443d6dd96f8
riyaprakash <74323094+riyaprakash@users.noreply.github.com>
2023-01-10 07:52:54 -0500

    login syntax

--- .github/workflows/login.py
+++ .github/workflows/login.py
@@ -1,21 +1,9 @@
 import requests
 import streamlit as st
-from streamlit_lottie import st_lottie
-from PIL import Image
 
 
 # Find more emojis here: https://www.webfx.com/tools/emoji-cheat-sheet/
 st.set_page_config(page_title="GISC Flight Suit Outfitter", page_icon= ":rocket:", layout="wide")
-
-
-# Use local CSS
-def local_css(file_name):
-    with open(file_name) as f:
-        st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
-
-
-local_css("style/style.css")
-
 
 # ---- HEADER SECTION ----
 with st.container():
