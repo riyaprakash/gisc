938373cb7c7219278eab05650a12fc290a75314f
riyaprakash <74323094+riyaprakash@users.noreply.github.com>
2023-01-11 00:51:26 -0500

    save

--- .github/workflows/pages/1_Customization.py
+++ .github/workflows/pages/1_Customization.py
@@ -1,6 +1,33 @@
 import requests
 import streamlit as st
 from PIL import Image
+from streamlit.components.v1 import html
+
+def nav_page(page_name, timeout_secs=3):
+    nav_script = """
+        <script type="text/javascript">
+            function attempt_nav_page(page_name, start_time, timeout_secs) {
+                var links = window.parent.document.getElementsByTagName("a");
+                for (var i = 0; i < links.length; i++) {
+                    if (links[i].href.toLowerCase().endsWith("/" + page_name.toLowerCase())) {
+                        links[i].click();
+                        return;
+                    }
+                }
+                var elasped = new Date() - start_time;
+                if (elasped < timeout_secs * 1000) {
+                    setTimeout(attempt_nav_page, 100, page_name, start_time, timeout_secs);
+                } else {
+                    alert("Unable to navigate to page '" + page_name + "' after " + timeout_secs + " second(s).");
+                }
+            }
+            window.addEventListener("load", function() {
+                attempt_nav_page("%s", new Date(), %d);
+            });
+        </script>
+    """ % (page_name, timeout_secs)
+    html(nav_script)
+
 
 image = Image.open('suit.jpg')
 
@@ -79,4 +106,5 @@ with rightcol:
             patches.form_submit_button("Save")
     # ---- FINISH ORDERING ----
     st.write("---")
-    st.button("Finish Ordering Suit")
+    if st.button("Finish Ordering Suit"):
+        nav_page("Cart")
--- .github/workflows/pages/4_Create_New_Account.py
+++ .github/workflows/pages/4_Create_New_Account.py
@@ -1,5 +1,31 @@
 import requests
 import streamlit as st
+from streamlit.components.v1 import html
+
+def nav_page(page_name, timeout_secs=3):
+    nav_script = """
+        <script type="text/javascript">
+            function attempt_nav_page(page_name, start_time, timeout_secs) {
+                var links = window.parent.document.getElementsByTagName("a");
+                for (var i = 0; i < links.length; i++) {
+                    if (links[i].href.toLowerCase().endsWith("/" + page_name.toLowerCase())) {
+                        links[i].click();
+                        return;
+                    }
+                }
+                var elasped = new Date() - start_time;
+                if (elasped < timeout_secs * 1000) {
+                    setTimeout(attempt_nav_page, 100, page_name, start_time, timeout_secs);
+                } else {
+                    alert("Unable to navigate to page '" + page_name + "' after " + timeout_secs + " second(s).");
+                }
+            }
+            window.addEventListener("load", function() {
+                attempt_nav_page("%s", new Date(), %d);
+            });
+        </script>
+    """ % (page_name, timeout_secs)
+    html(nav_script)
 
 # Find more emojis here: https://www.webfx.com/tools/emoji-cheat-sheet/
 st.set_page_config(page_title="GISC Flight Suit Outfitter", page_icon= ":rocket:", layout="wide")
@@ -26,4 +52,5 @@ with st.form("new_account"):
         -One Special Character """)
     st.text_input("")
     st.text_input("Re-enter your password")
-    st.form_submit_button("Create New Account")
+    if st.form_submit_button("Create New Account"):
+        nav_page("Home")
