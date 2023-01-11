c44aec3f24a7de6a2bc92fcbd87b6b290bb0e3ce
riyaprakash <74323094+riyaprakash@users.noreply.github.com>
2023-01-10 20:43:07 -0500

    center

--- .github/workflows/Home.py
+++ .github/workflows/Home.py
@@ -7,13 +7,13 @@ st.set_page_config(page_title="GISC Flight Suit Outfitter", page_icon= ":rocket:
 
 # ---- HEADER SECTION ----
 with st.container():
-    _, col2, _ = st.columns([1, 2, 1])
+    _, col2, _ = st.columns([1, 1, 1])
     with col2:
         st.title("Welcome to the Girl in Space Club Outfitter")
         st.subheader("Design Your Dream Flight Suit Here")
         st.write("##" "##")
     
-    col1, col2, col3 , col4, col5 = st.beta_columns(5)
+    col1, col2, col3 , col4, col5 = st.columns(5)
     with col1:
         pass
     with col2:
