f5740913416c2edd5dff173dd9ec21e0c72c4a6a
riyaprakash <74323094+riyaprakash@users.noreply.github.com>
2023-01-10 20:08:34 -0500

    home buttons

--- .github/workflows/Home.py
+++ .github/workflows/Home.py
@@ -9,3 +9,9 @@ st.set_page_config(page_title="GISC Flight Suit Outfitter", page_icon= ":rocket:
 with st.container():
     st.title("Welcome to the Girl in Space Club Outfitter")
     st.subheader("Design Your Dream Flight Suit Here")
+    left_column, right_column = st.columns(2)
+    with left_column:
+        st.button("Create New Suit")
+    with right_column:
+        st.button("View Cart")
+
