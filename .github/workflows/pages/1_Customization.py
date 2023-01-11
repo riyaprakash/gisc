0f2800ca709ed507b5371b12144e90136b67663a
riyaprakash <74323094+riyaprakash@users.noreply.github.com>
2023-01-11 08:05:37 -0500

    gap

--- .github/workflows/pages/1_Customization.py
+++ .github/workflows/pages/1_Customization.py
@@ -37,7 +37,7 @@ st.set_page_config(page_title="GISC Flight Suit Outfitter", page_icon= ":rocket:
 st.title("Flight Suit Customization")
 st.write("---")
 
-leftcol, rightcol = st.columns([5,4]);
+leftcol, rightcol = st.columns([5,4], "medium");
 
 with leftcol:
     st.write("##")
