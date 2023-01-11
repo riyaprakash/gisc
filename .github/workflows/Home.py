cd1a477e767fecdd4b2cee59f74e33dde617464c
riyaprakash <74323094+riyaprakash@users.noreply.github.com>
2023-01-11 00:47:47 -0500

    buttons

--- .github/workflows/Home.py
+++ .github/workflows/Home.py
@@ -1,9 +1,36 @@
 import requests
 import streamlit as st
+from streamlit.components.v1 import html
+
+def nav_page(page_name, timeout_secs=3):
+    nav_script = """
+        <script type="text/javascript">
+            function attempt_nav_page(page_name, start_time, timeout_secs) {
+                var links = window.parent.document.getElementsByTagName("a");
+                for (var i = 0; i < links.length; i++) {
+                    if (links[i].href.toLowerCase().endsWith("/" + page_name.toLowerCase())) {
+                        links[i].click();
+                        return;
+                    }
+                }
+                var elasped = new Date() - start_time;
+                if (elasped < timeout_secs * 1000) {
+                    setTimeout(attempt_nav_page, 100, page_name, start_time, timeout_secs);
+                } else {
+                    alert("Unable to navigate to page '" + page_name + "' after " + timeout_secs + " second(s).");
+                }
+            }
+            window.addEventListener("load", function() {
+                attempt_nav_page("%s", new Date(), %d);
+            });
+        </script>
+    """ % (page_name, timeout_secs)
+    html(nav_script)
 
 
 # Find more emojis here: https://www.webfx.com/tools/emoji-cheat-sheet/
 st.set_page_config(page_title="GISC Flight Suit Outfitter", page_icon= ":rocket:", layout="wide")
+
 
 # ---- HEADER SECTION ----
 with st.container():
@@ -12,6 +39,8 @@ with st.container():
     st.write('***')
     left_column, right_column = st.columns(2)
     with left_column:
-        st.button("Create New Suit")
+        if st.button("Create New Suit"):
+            nav_page("Customization")
     with right_column:
-        st.button("View Cart")
+        if st.button("View Cart"):
+            nav_page("Cart")
