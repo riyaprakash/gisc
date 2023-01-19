f2b7dfa6a89cc30e6be18288fa7bb94562d96406
riyaprakash <74323094+riyaprakash@users.noreply.github.com>
2023-01-19 13:02:02 -0500

    styles

--- .github/workflows/pages/2_Cart.py
+++ .github/workflows/pages/2_Cart.py
@@ -25,6 +25,7 @@ st.write("##")
 # Documention: https://formsubmit.co/ !!! CHANGE EMAIL ADDRESS !!!
 contact_form = """
 <form action="https://formsubmit.co/jnblume2@yahoo.com" method="POST">
+<link rel="stylesheet" href="styles.css">
     <input type="hidden" name="_captcha" value="false">
     <input type="text" name="name" placeholder="Your name" required>
     <input type="email" name="email" placeholder="Your email" required>
