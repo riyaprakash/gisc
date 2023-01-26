60b03683709e9c5e750e9b8196f5280a6aa04f21
riyaprakash <74323094+riyaprakash@users.noreply.github.com>
2023-01-26 12:44:02 -0500

    button

--- .github/workflows/pages/2_Cart.py
+++ .github/workflows/pages/2_Cart.py
@@ -68,7 +68,7 @@ with right:
     with open('style.css') as f:
             st.markdown(f'<style>{f.read()}</style>',unsafe_allow_html=True)
             st.markdown(
-            f'<a href={stripe_checkout} class="button" type="submit">Continue to Payment</a>',
+            f'<button href={stripe_checkout} type="submit">Continue to Payment</button>',
             unsafe_allow_html=True,
             )
 
