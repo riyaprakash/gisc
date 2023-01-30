837c207afd865c1600e8147431c5f188ca89684a
riyaprakash <74323094+riyaprakash@users.noreply.github.com>
2023-01-30 10:56:02 -0500

    email placeholder

--- .github/workflows/pages/2_Cart.py
+++ .github/workflows/pages/2_Cart.py
@@ -60,7 +60,7 @@ with left:
 
 # Continue to Payment button is disabled until email is entered
 with right:
-    email = st.text_input("Email", placeholder= "Required to Continue to Payment")
+    email = st.text_input("Email", placeholder= "Required")
     phone = st.text_input("Phone Number", placeholder="Optional")
     complete = True
     ##if email:
