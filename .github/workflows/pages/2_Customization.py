14b203083aef5279ce4f98730e125ec648e5575f
riyaprakash <74323094+riyaprakash@users.noreply.github.com>
2023-02-02 13:23:20 -0500

    switch pages

--- .github/workflows/pages/2_Customization.py
+++ .github/workflows/pages/2_Customization.py
@@ -5,7 +5,7 @@ import pandas as pd
 from csv import writer
 from PIL import Image
 from streamlit.components.v1 import html
-from streamlit_extras.switch_page_button import switch_page
+##from streamlit_extras.switch_page_button import switch_page
 from streamlit.source_util import get_pages
 
 def nav_page(page_name, timeout_secs=3):
@@ -36,6 +36,31 @@ def nav_page(page_name, timeout_secs=3):
 
 def balloons():
     st.balloons()
+
+@extra
+def switch_page(page_name: str):
+    from streamlit.runtime.scriptrunner import RerunData, RerunException
+    from streamlit.source_util import get_pages
+
+    def standardize_name(name: str) -> str:
+        return name.lower().replace("_", " ")
+
+    page_name = standardize_name(page_name)
+
+    pages = get_pages("streamlit_app.py")  # OR whatever your main page is called
+
+    for page_hash, config in pages.items():
+        if standardize_name(config["page_name"]) == page_name:
+            raise RerunException(
+                RerunData(
+                    page_script_hash=page_hash,
+                    page_name=page_name,
+                )
+            )
+
+    page_names = [standardize_name(config["page_name"]) for config in pages.values()]
+
+    raise ValueError(f"Could not find page {page_name}. Must be one of {page_names}")
     
 image = Image.open('flightsuit.jpg')
 
