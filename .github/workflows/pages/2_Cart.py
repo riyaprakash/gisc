86d2ec6d965365c68f7eb5a93d2d0ae2ffa25dca
jnblume2 <121893248+jnblume2@users.noreply.github.com>
2023-02-02 12:55:54 -0500

    fixed spacing

--- .github/workflows/pages/2_Cart.py
+++ .github/workflows/pages/2_Cart.py
@@ -100,10 +100,6 @@ st.write("##")
 if st.button("Customize Another Suit"):
     nav_page("Customization")
 
-# ---- CONTACT ----
-st.write("---")
-st.subheader("Contact us:")
-
 # ---- ENTER CART NUMBER ----
 st.header("Enter Your Cart Number to return to a previous cart")
 st.write("##")
@@ -111,6 +107,12 @@ st.write("##")
 # ---- DISPLAY CART NUMBER ----
 st.header("Your Cart Number: ")
 #add code to generate a random cart number for the csv or pull the old cart number out of the csv
+
+# ---- CONTACT ----
+st.write("---")
+st.subheader("Contact us:")
+
+
 
 # Documention: https://formsubmit.co/ !!! CHANGE EMAIL ADDRESS !!!
 with open('style.css') as f:
