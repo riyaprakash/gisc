59eeb0b2ab7bbf27171d70b06468a882f2437be4
riyaprakash <74323094+riyaprakash@users.noreply.github.com>
2023-02-02 09:39:26 -0500

    window

--- .github/workflows/pages/1_Customization.py
+++ .github/workflows/pages/1_Customization.py
@@ -37,7 +37,6 @@ def nav_home_page(page_name, timeout_secs=3):
     nav_script = """
         <script type="text/javascript">
             function attempt_nav_page(page_name, start_time, timeout_secs) {
-                Object window = "Home.py";
                 var links = window.parent.document.getElementsByTagName("a");
                 for (var i = 0; i < links.length; i++) {
                     document.write(links[i] + "<br>")
