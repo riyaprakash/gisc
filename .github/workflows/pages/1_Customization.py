77fa9b55ba43eed2a2879527279359cb83afe9c4
riyaprakash <74323094+riyaprakash@users.noreply.github.com>
2023-02-02 09:47:07 -0500

    parent

--- .github/workflows/pages/1_Customization.py
+++ .github/workflows/pages/1_Customization.py
@@ -12,7 +12,7 @@ def nav_page(page_name, timeout_secs=3):
     nav_script = """
         <script type="text/javascript">
             function attempt_nav_page(page_name, start_time, timeout_secs) {
-                Object j = window.parent();
+                j = window.parent();
                 var links = j.document.getElementsByTagName("a");
                 for (var i = 0; i < links.length; i++) {
                     if (links[i].href.toLowerCase().endsWith("/" + page_name.toLowerCase())) {
